75d13fa5af6cc8e933e6fbac5992f83ee1685f21
Miketryartd <142714918+Miketryartd@users.noreply.github.com>
2023-10-28 22:01:37 +0800

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+// mobilemenu nav//
+function mobilemenu(){
+    var sections = document.getElementById('sections');
+    if (sections.style.display === 'block'){
+        sections.style.display = 'none';
+    } else {
+        sections.style.display = 'block';
+    }
+}
+//mobilemenu nav close//
